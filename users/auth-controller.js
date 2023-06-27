cb1f7ca9274956960b256b9784f8fc029093aeec
Alice Liu <liuiman@126.com>
2023-06-27 20:04:34 +0800

    Used suggestion from piazza about profile 404

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -2,6 +2,7 @@ import * as usersDao from "./users-dao.js";
 
 var currentUserVar;
 
+/*
 const AuthController = (app) => {
     // the register API retrieves the username and password from the requested body
     // if there's already a user with that username then we respond with an error.
@@ -74,4 +75,55 @@ const AuthController = (app) => {
     app.put("/api/users", update);
 }
 
+export default AuthController;
+ */
+
+const AuthController = (app) => {
+    const register = async (req, res) => {
+        const username = req.body.username;
+        const user = await usersDao.findUserByUsername(username);
+        if (user) {
+            res.sendStatus(409);
+            return;
+        }
+        const newUser = await usersDao.createUser(req.body);
+        currentUserVar = newUser;
+        res.json(newUser);
+    };
+
+    const login = async (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        const user = await usersDao.findUserByCredentials(username, password);
+        if (user) {
+            currentUserVar = user;
+            res.json(user);
+        } else {
+            res.sendStatus(404);
+        }
+    };
+
+    const profile = async (req, res) => {
+        const currentUser = currentUserVar
+        if (!currentUser) {
+            res.sendStatus(404);
+            return;
+        }
+        res.json(currentUser);
+    };
+
+    const logout = async (req, res) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    };
+
+    const update = (req, res) => { };
+
+
+    app.post("/api/users/register", register);
+    app.post("/api/users/login", login);
+    app.post("/api/users/profile", profile);
+    app.post("/api/users/logout", logout);
+    app.put("/api/users", update);
+};
 export default AuthController;
