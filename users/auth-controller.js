b6a5ed026c76b18755d58ef79e56401f9af7b592
Alice Liu <71456398+alice0217@users.noreply.github.com>
2023-06-27 08:26:16 -0400

    Update auth-controller.js only remotely

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -2,7 +2,6 @@ import * as usersDao from "./users-dao.js";
 
 var currentUserVar;
 
-/*
 const AuthController = (app) => {
     // the register API retrieves the username and password from the requested body
     // if there's already a user with that username then we respond with an error.
@@ -53,7 +52,8 @@ const AuthController = (app) => {
     };
 
     const update = (req, res) => {
-        const currentUser = currentUserVar;
+        // const currentUser = currentUserVar;
+        const currentUser = req.session["currentUser"];
         if (!currentUser) {
             res.sendStatus(404);
         }
@@ -61,7 +61,8 @@ const AuthController = (app) => {
         const currentUserId = currentUser._id;
         const updatedUser = usersDao.updateUser(currentUserId, updateContent);
         if (updatedUser) {
-            currentUserVar = updatedUser;
+            // currentUserVar = updatedUser;
+            req.session["currentUser"] = updatedUser;
             res.json(updatedUser);
         } else {
             res.sendStatus(404);
@@ -75,68 +76,4 @@ const AuthController = (app) => {
     app.put("/api/users", update);
 }
 
-export default AuthController;
- */
-
-const AuthController = (app) => {
-    const register = async (req, res) => {
-        const username = req.body.username;
-        const user = await usersDao.findUserByUsername(username);
-        if (user) {
-            res.sendStatus(409);
-            return;
-        }
-        const newUser = await usersDao.createUser(req.body);
-        currentUserVar = newUser;
-        res.json(newUser);
-    };
-
-    const login = async (req, res) => {
-        const username = req.body.username;
-        const password = req.body.password;
-        const user = await usersDao.findUserByCredentials(username, password);
-        if (user) {
-            currentUserVar = user;
-            res.json(user);
-        } else {
-            res.sendStatus(404);
-        }
-    };
-
-    const profile = async (req, res) => {
-        const currentUser = currentUserVar
-        if (!currentUser) {
-            res.sendStatus(404);
-            return;
-        }
-        res.json(currentUser);
-    };
-
-    const logout = async (req, res) => {
-        req.session.destroy();
-        res.sendStatus(200);
-    };
-
-    const update = (req, res) => {
-        const currentUser = currentUserVar;
-        if (!currentUser) {
-            res.sendStatus(404);
-        }
-        const updateContent = req.body;
-        const currentUserId = currentUser._id;
-        const updatedUser = usersDao.updateUser(currentUserId, updateContent);
-        if (updatedUser) {
-            currentUserVar = updatedUser;
-            res.json(updatedUser);
-        } else {
-            res.sendStatus(404);
-        }
-    };
-
-    app.post("/api/users/register", register);
-    app.post("/api/users/login", login);
-    app.post("/api/users/profile", profile);
-    app.post("/api/users/logout", logout);
-    app.put("/api/users", update);
-};
 export default AuthController;
